a3830bfa4d0efbd3f78ef85ecc1bcf7ed3197e66
kahne-zero <kaseyforce@outlook.com>
2025-07-07 00:55:30 -0400

    changed command line interface

--- src/cli/klc.c
+++ src/cli/klc.c
@@ -12,22 +12,22 @@ int main(int argc, char* arg[]) {
         printf("`%s -h` for help.\n", arg[0]);
         return 1;
     } else if (!strcmp(arg[1], "-h")) {
-        printf("# Kalandra Language Compiler\n**See `## Notes` in `readme.md`.**\n\n"
-            "## Compiling\n"
-            "- `-s` : Link source code path\n"
-            "- `-o` : Link output path\n"
-            "- `-a` : Output only Intel 8086/8088 assembly\n\n"
-            "## Kalandra CLI\n"
-            "- `-v` : Show Kalandra Langauge Compiler (KLC) version details\n\n"
+        printf("Kalandra Language Compiler\n\n"
+            "--- Compiling: ---\n"
+            " -s : Link source code path\n"
+            " -o : Link output path\n"
+            " -a : Output only Intel 8086/8088 assembly\n\n"
+            "--- Kalandra CLI ---\n"
+            " -v : Show Kalandra Langauge Compiler (KLC) version details\n\n"
         );
         return 0;
     } else if (!strcmp(arg[1], "-v")) {
-        printf("# Kalandra Language Compiler\n**See `## Notes` in `readme.md`.**\n\n"
-            "## Version\n"
+        printf("Kalandra Language Compiler\n\n"
+            "--- Version: ---\n"
             "- Syntax Version: KPL21\n"
             "- Compiler Version: 0.0.0 (Linux-x86_64)\n\n"
-            "## Compiler Architectures\n"
-            "- `-m 86` : Compile to Intel 8086/8088\n\n"
+            "--- Compiler Architectures: ---\n"
+            " -m 86 : Compile to Intel 8086/8088\n\n"
         );
         return 0;
     }
@@ -37,15 +37,15 @@ int main(int argc, char* arg[]) {
     bool select_d = 0;
     for (int i = 1; i < argc; i++) {
         if (!strcmp(arg[i], "-h")) {
-            printf("**WARN:** `-h` must be at beginning, **ignoring -h**\n");
+            printf("WARN: -h must be at beginning, ignoring -h\n");
             continue;
         } else if (!strcmp(arg[i], "-v")) {
-            printf("**WARN:** `-v` must be at beginning, **ingoring -v**\n");
+            printf("WARN: -v must be at beginning, ingoring -v\n");
             continue;
         }
         if (!strcmp(arg[i], "-s")) {
             if (select_s) {
-                printf("**WARN:** Source file already selected, **ignoring duplicate -s**\n");
+                printf("WARN: Source file already selected, ignoring duplicate -s\n");
                 i++;
                 continue;
             }
@@ -54,14 +54,14 @@ int main(int argc, char* arg[]) {
                 select_s = true;
                 i++;
             } else {
-                printf("**FATAL:** Missing argument after -s\n");
+                printf("FATAL: Missing argument after -s\n");
                 return 1;
             }
             continue;
         }
         if (!strcmp(arg[i], "-o")) {
             if (select_o) {
-                printf("**WARN:** Output path already selected, **ignoring duplicate -o**\n");
+                printf("WARN: Output path already selected, ignoring duplicate -o\n");
                 i++;
                 continue;
             }
@@ -70,14 +70,14 @@ int main(int argc, char* arg[]) {
                 select_s = true;
                 i++;
             } else {
-                printf("**FATAL:** Missing argument after -o\n");
+                printf("FATAL: Missing argument after -o\n");
                 return 1;
             }
             continue;
         }
         if (!strcmp(arg[i], "-a")) {
             if (select_o) {
-                printf("**WARN:** Assembly output already selected, **ignoring duplicate -a**\n");
+                printf("WARN: Assembly output already selected, ignoring duplicate -a\n");
                 continue;
             }
             cfg.outtype = 1;
@@ -85,7 +85,7 @@ int main(int argc, char* arg[]) {
         }
         if (!strcmp(arg[i], "-o")) {
             if (select_o) {
-                printf("**WARN:** Output path already selected, **ignoring duplicate -o**\n");
+                printf("WARN: Output path already selected, ignoring duplicate -o\n");
                 i++;
                 continue;
             }
@@ -94,25 +94,25 @@ int main(int argc, char* arg[]) {
                 select_s = true;
                 i++;
             } else {
-                printf("**FATAL:** Missing argument after -o\n");
+                printf("FATAL: Missing argument after -o\n");
                 return 1;
             }
             continue;
         }
         if (!strcmp(arg[i], "-d")) {
             if (select_d) {
-                printf("**WARN:** Debug already enabled, **ignoring duplicate -d**\n");
+                printf("WARN: Debug already enabled, ignoring duplicate -d\n");
                 continue;
             }
             cfg.e_debug = 1;
             select_d = true;
             continue;
         }
-        printf("**FATAL:** Invalid argument (%d, '%s')\n", i, arg[i]);
+        printf("FATAL: Invalid argument (%d, '%s')\n", i, arg[i]);
         return 1;
     }
     if (!select_s) {
-        printf("**FATAL:** Source never given\n");
+        printf("FATAL: Source never provided (missing -s)\n");
         return 1;
     } else if (!select_o) {
         cfg.out_path = cfg.in_path;
